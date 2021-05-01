21b28af0a620c73f81aa793da6bd283a7ef21ff1
JonathanDPotter <jonathan.d.potter@outlook.com>
2021-05-01 05:47:28 -0400

    starting fresh

--- src/index.js
+++ src/index.js
@@ -1,16 +1,2 @@
 import "./style.css";
-import setup from "/src/setup.js";
 
-const state = {
-  projects: [],
-  addItem(project) {
-    this.projects.push(project);
-  },
-  removeItem(i) {
-    this.projects.splice(i, 1);
-  },
-};
-
-setup.setup();
-
-export default state ;
